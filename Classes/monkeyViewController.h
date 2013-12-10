2098c110b97fc3c4bad2e0425d6341d46979b3b6
Zack Garbowitz <zgarbowitz@gmail.com>
2013-12-10 10:36:09 -0500

    Adding initial FIndTheMonkey game without Skillz.

---
+++ Classes/monkeyViewController.h
@@ -0,0 +1,30 @@
+//
+//  monkeyViewController.h
+//  monkey
+//
+//  Created by Robert Diamond on 4/9/11.
+//  Copyright 2011 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface monkeyViewController : UIViewController<AVAudioPlayerDelegate> {
+	NSUInteger hiddenLocation;
+	NSTimer *clock;
+	NSUInteger elapsed_seconds;
+	AVAudioPlayer *avp;
+	AVAudioPlayer *win;
+}
+
+@property (nonatomic, assign) IBOutlet UIButton *startButton;
+@property (nonatomic, assign) IBOutlet UITextField *timeRemaining;
+@property (nonatomic, assign) IBOutlet UILabel *where;
+
+@property (nonatomic, assign) IBOutlet UIButton *resetButton;
+
+- (IBAction)startGameButton:(id)sender;
+- (void)guess:(id)sender;
+- (void)tick:(NSTimer *)timer;
+@end
+
